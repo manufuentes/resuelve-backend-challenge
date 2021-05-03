52cc660fd0359029c98c7cfe75801d950e6ea5ae
Emmanuel Fuentes <fuencort@gmail.com>
2021-05-03 16:32:46 -0500

    Implementación de bonus (rankings de goleo customizables por equipo)

--- calcular_sueldos.js
+++ calcular_sueldos.js
@@ -1,11 +1,5 @@
 const R = require('ramda');
 
-/**
- * - Clasificar por equipos (Convertir lista de jugadores en listas de jugadores por equipo)
- * - Calcular rendimiento grupal
- * - Calcular rendimiento individual
- * - Calcular sueldo completo
- */
     
 const groupByTeams = JSONObj => {
     const getTeamsNames = JSONObj => R.pluck('equipo', JSONObj.jugadores);
@@ -17,15 +11,26 @@ const groupByTeams = JSONObj => {
     return R.compose(updateJSONObj, groupPlayersByTeam, removeDuplicatedTeamsNames, getTeamsNames)(JSONObj);
 };
 
-const setTeamsMinGoals = JSONObj => {
-    const tier2Goals = R.cond([
-        [R.equals('A'), R.always(5)],
-        [R.equals('B'), R.always(10)],
-        [R.equals('C'), R.always(15)],
-        [R.equals('Cuauh'), R.always(20)]
-    ]); 
+const setTeamsMinGoals = (config, JSONObj) => {
 
-    const setMinGoalsByPlayer = player => R.set(R.lensProp('minimo_goles'), tier2Goals(player.nivel), player);
+    const setMinGoalsByPlayer = player => {
+        const rank2MinGoals = player => {
+            const mappedConfig = R.map(n => R.pair(R.equals(n[0]), R.always(n[1])), R.toPairs(player.rankeo_config.rankeo));
+            return R.cond(mappedConfig)(player.nivel);
+        }; 
+
+        const setPlayerConfig = player => {
+            const customConfig = R.filter(R.pathEq(['nombre'], player.equipo), config.equipos);
+            const defaultConfig = R.filter(R.pathEq(['nombre'], 'Resuelve FC'), config.equipos);
+            const setConfig = config => R.set(R.lensProp('rankeo_config'), R.head(config), player);
+
+            return R.ifElse(R.isEmpty, R.always(setConfig(defaultConfig)), R.always(setConfig(customConfig)))(customConfig);
+        };
+
+        const setPlayerMinGoals = player => R.set(R.lensProp('minimo_goles'), rank2MinGoals(player), player);
+        return R.compose(setPlayerMinGoals, setPlayerConfig)(player);
+    };
+
     const setMinGoalsByTeam = team => R.map(setMinGoalsByPlayer, team);
     const setMinGoals = teams => R.map(setMinGoalsByTeam, teams);
 
@@ -33,10 +38,12 @@ const setTeamsMinGoals = JSONObj => {
 };
 
 const setTeamsThrowput = JSONObj => {
+
     const sumTeamMinGoals = team => R.sum(R.pluck('minimo_goles', team));
     const sumTeamScoredGoals = team => R.sum(R.pluck('goles', team));
     const getTeamThrowput = team => R.divide(sumTeamScoredGoals(team), sumTeamMinGoals(team));
     const truncateTeamThrowput = team => R.when(R.gte(R.__, 1), R.always(1))(getTeamThrowput(team));
+
     const setThrowputByTeam = team => R.map(R.set(R.lensProp('alcance_equipo'), truncateTeamThrowput(team)), team);
     const setThrowput = teams => R.map(setThrowputByTeam, teams);
 
@@ -44,8 +51,10 @@ const setTeamsThrowput = JSONObj => {
 };
 
 const setIndividualsThrowput = JSONObj => {
+
     const getPlayerThrowput = player => R.divide(player.goles, player.minimo_goles);
     const truncatePlayerThrowput = player => R.when(R.gte(R.__, 1), R.always(1))(getPlayerThrowput(player));
+
     const setIThrowputByPlayer = player => R.set(R.lensProp('alcance_individual'), truncatePlayerThrowput(player), player);
     const setIThrowputByTeam = team => R.map(setIThrowputByPlayer, team);
     const setIThrowput = teams => R.map(setIThrowputByTeam, teams);
@@ -54,7 +63,9 @@ const setIndividualsThrowput = JSONObj => {
 };
 
 const setSalary = JSONObj => {
+
     const getBonus = player => R.multiply(player.bono, R.mean([player.alcance_equipo, player.alcance_individual]));
+
     const setCompleteSalaryByPlayer = player => R.set(R.lensProp('sueldo_completo'), R.sum([player.sueldo, getBonus(player)]), player);
     const setCompleteSalaryByTeam = team => R.map(setCompleteSalaryByPlayer, team);
     const setCompleteSalary = teams => R.map(setCompleteSalaryByTeam, teams);
@@ -62,5 +73,5 @@ const setSalary = JSONObj => {
     return R.set(R.lensProp('jugadores'), R.flatten(setCompleteSalary(JSONObj.jugadores)), JSONObj);
 };
 
-const setSalaries = R.compose(setSalary, setIndividualsThrowput, setTeamsThrowput, setTeamsMinGoals, groupByTeams);
+const setSalaries = (JSONObj, config) => R.compose(setSalary, setIndividualsThrowput, setTeamsThrowput, R.curry(setTeamsMinGoals)(config), groupByTeams)(JSONObj);
 exports.setSalaries = setSalaries;
--- server.js
+++ server.js
@@ -2,16 +2,27 @@ const express = require('express');
 const salaryCalculator = require('./calcular_sueldos');
 
 const app = express();
+
+let config = require('./config.json');
+
 app.use(express.json());
  
 app.post('/get_salaries', (req, res) => {
     const JSONObj = req.body;
-    res.send(salaryCalculator.setSalaries(JSONObj));
+    res.send(salaryCalculator.setSalaries(JSONObj, config));
 });
  
-app.post('/get_salaries_w_custom_throwput', (req, res) => {
-    const JSONObj = req.body;
-    res.send(salaryCalculator.setSalaries(JSONObj));
+// TODO: IMPLEMENTAR JSON VERIFY
+app.get('/reset_config', (req, res) => {
+    config = require('./config.json');
+    res.send('ok');
 });
+
+app.get('/config', (req, res) => res.send(config));
+app.post('/config', (req, res) => {
+    config = req.body;
+    res.send('ok');
+});
+
 
 app.listen(3000)
