ea4c5cca7ea8f60df462e8c65f6a7bcf55b2d40e
Emmanuel Fuentes <fuencort@gmail.com>
2021-05-04 16:43:42 -0500

    Refactorización de endpoints, se prepara para producción

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const salaryCalculator = require('./calcular_sueldos');
+const { body, validationResult } = require('express-validator');
 
 const app = express();
 
@@ -7,22 +8,43 @@ let config = require('./config.json');
 
 app.use(express.json());
  
-app.post('/get_salaries', (req, res) => {
-    const JSONObj = req.body;
-    res.send(salaryCalculator.setSalaries(JSONObj, config));
+app.post('/get_salaries',
+    body('jugadores').isArray({ min: 1 }),
+    (req, res) => {
+        
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+
+        try {
+            const JSONObj = req.body;
+            const salaries = salaryCalculator.setSalaries(JSONObj, config);
+            return res.status(200).send(salaries);
+        }
+        catch(err) { return res.status(501).send(err); } 
 });
  
-// TODO: IMPLEMENTAR JSON VERIFY
 app.get('/reset_config', (req, res) => {
-    config = require('./config.json');
-    res.send('ok');
+    try { 
+        config = require('./config.json'); 
+        return res.status(200).send(config);
+    }
+    catch(err) { return res.status(501).send(err); } 
 });
 
-app.get('/config', (req, res) => res.send(config));
-app.post('/config', (req, res) => {
-    config = req.body;
-    res.send('ok');
-});
+app.get('/config', (req, res) => res.status(200).send(config));
+app.post('/config',
+    body('equipos').isArray({ min: 1 }),
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+
+        try{
+            config = req.body; 
+            return res.status(200).send({"success": true}); 
+        }
+        catch(err) { return res.status(501).send(err); }
+    }
+);
 
 
 app.listen(3000)
